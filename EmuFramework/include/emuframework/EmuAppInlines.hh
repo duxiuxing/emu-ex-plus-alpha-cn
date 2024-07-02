86453a00f790b97e31e1fc54a9ca27cbae416283
duxiuxing <duxiuxing@foxmail.com>
2024-07-03 00:33:54 +0800

    Add UI_TEXT macro

--- EmuFramework/include/emuframework/EmuAppInlines.hh
+++ EmuFramework/include/emuframework/EmuAppInlines.hh
@@ -31,7 +31,7 @@ class EmuSystem;
 
 std::u16string_view EmuApp::mainViewName()
 {
-	return u"" CONFIG_APP_NAME " " IMAGINE_VERSION;
+	return UI_TEXT(u"" CONFIG_APP_NAME " " IMAGINE_VERSION);
 }
 
 bool EmuApp::hasGooglePlayStoreFeatures()
