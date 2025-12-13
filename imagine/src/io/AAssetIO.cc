d5a3261aea194282dfed32c2182861ab0ecfa4f9
R-Sam-1980 <duxiuxing@gmail.com>
2025-12-14 07:16:27 +0800

    fix build error

--- imagine/src/io/AAssetIO.cc
+++ imagine/src/io/AAssetIO.cc
@@ -13,6 +13,7 @@
 	You should have received a copy of the GNU General Public License
 	along with Imagine.  If not, see <http://www.gnu.org/licenses/> */
 
+#include <imagine/config/defs.hh>
 #include <android/asset_manager.h>
 #include <unistd.h>
 #include <sys/mman.h>
