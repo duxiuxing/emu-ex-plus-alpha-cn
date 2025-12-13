54803c5689bfc569725199135b3324e19a867bf3
R-Sam-1980 <duxiuxing@gmail.com>
2025-12-14 07:01:31 +0800

    fix build error

--- imagine/src/io/ArchiveIO.cc
+++ imagine/src/io/ArchiveIO.cc
@@ -13,6 +13,7 @@
 	You should have received a copy of the GNU General Public License
 	along with Imagine.  If not, see <http://www.gnu.org/licenses/> */
 
+#include <imagine/config/defs.hh>
 #include <imagine/util/macros.h>
 #include <archive.h>
 #include <archive_entry.h>
