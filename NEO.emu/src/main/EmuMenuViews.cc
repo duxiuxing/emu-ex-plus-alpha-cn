3bd898f898084c6481c5223decde2f9cbb0e326d
R-Sam-1980 <duxiuxing@gmail.com>
2026-05-16 13:40:34 +0800

    NEO.emu.cn # to G

--- NEO.emu/src/main/EmuMenuViews.cc
+++ NEO.emu/src/main/EmuMenuViews.cc
@@ -255,69 +255,233 @@ struct RomListEntry
 };
 
 constexpr RomListEntry romlist[]
-{
-	{ "2020bb", 0 },
+{	
+	{ "eightman", 0 },	// # - 8号超人
+	{ "neocup98", 0 },	// # - 98机皇杯 胜利之路
+	{ "2020bb", 0 },	// # - 2020超级棒球
 	{ "2020bba", 0 },
 	{ "2020bbh", 0 },
-	{ "3countb", 0 },
-	{ "alpham2", 0 },
-	{ "androdun", 0 },
-	{ "aodk", 0 },
-	{ "aof", 0 },
-	{ "aof2", 0 },
-	{ "aof2a", 0 },
-	{ "aof3", 0 },
-	{ "aof3k", 0 },
-	{ "bakatono", 0 },
-	{ "bangbead", 0 },
-	{ "bbbuster", 0 },
-	{ "bjourney", 0 },
-	{ "blazstar", 0 },
-	{ "breakers", 0 },
-	{ "breakrev", 0 },
-	{ "bstars", 0 },
-	{ "bstars2", 0 },
-	{ "burningf", 0 },
-	{ "burningfh", 0 },
-	{ "crsword", 0 },
-	{ "ct2k3sa", 1 },
-	{ "ct2k3sp", 1 },
-	{ "cthd2003", 1 },
-	{ "ctomaday", 0 },
-	{ "cyberlip", 0 },
-	{ "diggerma", 0 },
-	{ "doubledr", 0 },
-	{ "eightman", 0 },
-	{ "fatfursa", 0 },
+	{ "alpham2", 0 },	// A - 阿尔法任务2
+	{ "ninjamas", 0 },	// B - 霸王忍法帖
+	{ "bstars", 0 },	// B - 棒球之星专业版
+	{ "bstars2", 0 },	// B - 棒球之星2
+	{ "breakers", 0 },	// B - 爆裂人
+	{ "breakrev", 0 },	// B - 爆裂人复仇
+	{ "bbbuster", 1 },	// B - 爆炸克星 (有bug)
+	{ "wjammers", 0 },	// C - 超级飞碟球
+	{ "pspikes2", 0 },	// C - 超级排球2
+	{ "gowcaizr", 0 },	// C - 超人学园钢帝王
+	{ "ironclad", 0 },	// C - 超铁战机
+	{ "ironclado", 0 },
+	{ "pnyaa", 0 },		// C - 宠物方块
+	{ "s1945p", 0 },	// D - 打击者1945加强版
+	{ "turfmast", 0 },	// D - 大联盟高尔夫
+	{ "ssideki", 0 },	// D - 得点王1
+	{ "ssideki2", 0 },	// D - 得点王2
+	{ "ssideki3", 0 },	// D - 得点王3
+	{ "ssideki4", 0 },	// D - 得点王4
+	{ "superspy", 0 },	// D - 谍报飞龙
+	{ "tpgolf", 0 },	// D - 顶尖高尔夫
+	{ "tophuntr", 0 },	// D - 顶尖猎人
+	{ "tophuntra", 0 },
+	{ "popbounc", 0 },	// D - 动物方块
+	{ "neomrdo", 0 },	// D - 嘟先生
+	{ "flipshot", 0 },	// D - 盾牌大战
+	{ "fatfury1", 0 },	// E - 饿狼传说1 宿命之战
+	{ "fatfury2", 1 },	// E - 饿狼传说2
+	{ "fatfursa", 0 },	// E - 饿狼传说特别版
 	{ "fatfursp", 0 },
-	{ "fatfury1", 0 },
-	{ "fatfury2", 1 },
-	{ "fatfury3", 0 },
-	{ "fbfrenzy", 0 },
-	{ "fightfev", 0 },
-	{ "fightfeva", 0 },
-	{ "flipshot", 0 },
-	{ "fswords", 0 },
-	{ "galaxyfg", 0 },
-	{ "ganryu", 0 },
-	{ "garou", 0 },
+	{ "fatfury3", 0 },	// E - 饿狼传说3 远古征战
+	{ "garou", 0 },		// E - 饿狼传说 狼之印记
 	{ "garoubl", 0 },
 	{ "garouo", 0 },
 	{ "garoup", 0 },
-	{ "ghostlop", 0 },
-	{ "goalx3", 0 },
-	{ "gowcaizr", 0 },
-	{ "gpilots", 0 },
+	{ "ctomaday", 0 },	// F - 蕃茄超人
+	{ "fbfrenzy", 0 },	// F - 疯狂美式足球
+	{ "ridhero", 1 },	// F - 风速英雄 (有bug)
+	{ "ridheroh", 1 },
+	{ "savagere", 0 },	// F - 风云默示录
+	{ "kizuna", 0 },	// F - 风云默示录 超级组队战
+	{ "kotm", 0 },		// G - 怪兽之王1
+	{ "kotmh", 0 },
+	{ "kotm2", 0 },		// G - 怪兽之王2
+	{ "ghostlop", 0 },	// G - 鬼精灵投球手
+	{ "mslug", 0 },		// H - 合金弹头1
+	{ "mslug2", 0 },	// H - 合金弹头2
+	{ "mslugx", 0 },	// H - 合金弹头X
+	{ "mslug3", 0 },	// H - 合金弹头3
+	{ "mslug3b6", 0 },
+	{ "mslug3h", 0 },
+	{ "mslug4", 0 },	// H - 合金弹头4
+	{ "ms4plus", 0 },
+	{ "mslug5", 0 },	// H - 合金弹头5
+	{ "mslug5h", 0 },
+	{ "ms5pcb", 1 },
+	{ "ms5plus", 1 },
+	{ "totcarib", 0 },	// H - 黑珍珠部队
+	{ "maglord", 0 },	// H - 幻魔大战
+	{ "maglordh", 0 },
+	{ "wakuwak7", 0 },	// H - 火热火热7
+	{ "pulstar", 0 },	// H - 惑星战机
+	{ "roboarmy", 0 },	// J - 机甲神兵
+	{ "socbrawl", 0 },	// J - 机器人足球
+	{ "socbrawla", 0 },
+	{ "trally", 0 },	// J - 极速英豪
+	{ "mosyougi", 0 },	// J - 将棋达人
+	{ "strhoop", 0 },	// J - 街头篮球
+	{ "puzzledp", 0 },	// J - 解谜气泡
+	{ "puzzldpr", 0 },	// J - 解谜气泡R
+	{ "karnovr", 0 },	// K - 卡诺夫的复仇
+	{ "pgoal", 0 },		// K - 开心射门 5对5迷你足球
+	{ "overtop", 0 },	// K - 跨越巅峰
+	{ "fightfev", 0 },	// K - 狂热格斗
+	{ "fightfeva", 0 },
+	{ "neodrift", 0 },	// L - 拉力赛车 新的技术
+	{ "bjourney", 0 },	// L - 蓝色之旅
+	{ "lbowling", 0 },	// L - 联盟保龄球
+	{ "rotd", 1 },		// L - 龙吼 (有bug)
+	{ "aof", 0 },		// L - 龙虎之拳1
+	{ "aof2", 0 },		// L - 龙虎之拳2
+	{ "aof2a", 0 },
+	{ "aof3", 0 },		// L - 龙虎之拳3
+	{ "aof3k", 0 },
+	{ "minasan", 0 },	// M - 麻将大富翁
+	{ "mahretsu", 0 },	// M - 麻将狂列传
+	{ "gururin", 0 },	// M - 梦幻天堂
+	{ "twinspri", 0 },	// M - 梦幻小妖精
+	{ "legendos", 0 },	// M - 明日之丈传说
+	{ "magdrop2", 0 },	// M - 魔法微量球2
+	{ "magdrop3", 0 },	// M - 魔法微量球3
+	{ "gpilots", 0 },	// M - 魔鬼飞行员
 	{ "gpilotsh", 0 },
-	{ "gururin", 0 },
-	{ "ironclad", 0 },
-	{ "ironclado", 0 },
-	{ "irrmaze", 1 },
-	{ "janshin", 0 },
-	{ "jockeygp", 1 },
-	{ "joyjoy", 0 },
-	{ "kabukikl", 0 },
-	{ "karnovr", 0 },
+	{ "pbobblen", 0 },	// P - 泡泡龙1
+	{ "pbobblena", 0 },
+	{ "pbobbl2n", 0 },	// P - 泡泡龙2
+	{ "bangbead", 0 },	// P - 碰碰球
+	{ "miexchng", 0 },	// Q - 钱币对抗赛
+	{ "spinmast", 0 },	// Q - 强尼大冒险
+	{ "kof94", 0 },		// Q - 拳皇94
+	{ "kof95", 0 },		// Q - 拳皇95
+	{ "kof95h", 0 },
+	{ "kof96", 0 },		// Q - 拳皇96
+	{ "kof96h", 0 },
+	{ "kof97", 0 },		// Q - 拳皇97
+	{ "kof97a", 0 },
+	{ "kog", 1 },
+	{ "kof97pls", 0 },
+	{ "kof98", 0 },		// Q - 拳皇98
+	{ "kof98k", 0 },
+	{ "kof98n", 0 },
+	{ "kof99", 0 },		// Q - 拳皇99
+	{ "kof99a", 0 },
+	{ "kof99e", 0 },
+	{ "kof99n", 0 },
+	{ "kof99p", 0 },
+	{ "kof2000", 0 },	// Q - 拳皇2000
+	{ "kof2000n", 0 },
+	{ "kof2001", 0 },	// Q - 拳皇2001
+	{ "kof2001h", 0 },
+	{ "kof2002", 0 },	// Q - 拳皇2002
+	{ "kof2002b", 1 },
+	{ "kof2003", 0 },	// Q - 拳皇2003
+	{ "kof2003h", 0 },
+	{ "svc", 0 },		// Q - 拳皇对街霸
+	{ "svcboot", 1 },
+	{ "svcpcb", 1 },
+	{ "svcpcba", 1 },
+	{ "svcplus", 1 },
+	{ "svcplusa", 1 },
+	{ "svcsplus", 1 },
+	{ "quizkof", 0 },	// Q - 拳皇问答
+	{ "janshin", 0 },	// Q - 雀神传说
+	{ "sdodgeb", 0 },	// R - 热血超级躲避球
+	{ "burningf", 0 },	// R - 热血快打
+	{ "burningfh", 0 },
+	{ "ncombat", 0 },	// R - 忍者大战
+	{ "ncombath", 0 },
+	{ "ncommand", 0 },	// R - 忍者指令
+	{ "stakwin", 0 },	// S - 赛马大亨1
+	{ "stakwin2", 0 },	// S - 赛马大亨2
+	{ "jockeygp", 1 },	// S - 赛马大奖赛 (有Bug)
+	{ "3countb", 0 },	// S - 三回合较量
+	{ "bakatono", 0 },	// S - 傻瓜殿下麻将漫游记
+	{ "blazstar", 0 },	// S - 闪亮之星
+	{ "goalx3", 0 },	// S - 射门！射门！射门！
+	{ "ragnagrd", 0 },	// S - 神凰拳
+	{ "zintrckb", 0 },	// S - 生物泡泡
+	{ "crsword", 0 },	// S - 圣十字剑
+	{ "samsho", 0 },	// S - 侍魂1
+	{ "samshoh", 0 },
+	{ "samsho2", 0 },	// S - 侍魂2 霸王丸地狱变
+	{ "samsho3", 0 },	// S - 侍魂3 斩红郎无双剑
+	{ "samsho3h", 0 },
+	{ "fswords", 0 },
+	{ "samsho4", 0 },	// S - 侍魂4 天草降临
+	{ "samsho5", 0 },	// S - 侍魂5 零
+	{ "samsho5b", 1 },
+	{ "samsho5h", 0 },
+	{ "samsh5sp", 0 },	// S - 侍魂5 特别版
+	{ "samsh5sph", 0 },
+	{ "samsh5spn", 0 },
+	{ "wh1", 0 },		// S - 世界英雄1
+	{ "wh1h", 0 },
+	{ "wh1ha", 0 },
+	{ "wh2", 0 },		// S - 世界英雄2
+	{ "wh2j", 0 },
+	{ "wh2jh", 0 },
+	{ "whp", 0 },		// S - 世界英雄完美版
+	{ "viewpoin", 0 },	// S - 视觉战机
+	{ "doubledr", 0 },	// S - 双截龙
+	{ "vliner", 1 },	// S - 水果机 (有Bug)
+	{ "vlinero", 1 },
+	{ "sonicwi2", 0 },	// S - 四国战机2
+	{ "sonicwi3", 0 },	// S - 四国战机3
+	{ "tws96", 0 },		// T - Tecmo世界杯足球96
+	{ "kabukikl", 0 },	// T - 天外魔境真传
+	{ "aodk", 0 },		// T - 痛快进行曲
+	{ "shocktro", 0 },	// T - 突击骑兵1
+	{ "shocktra", 0 },
+	{ "shocktr2", 0 },	// T - 突击骑兵2 第二分队
+	{ "lans2004", 1 },
+	{ "diggerma", 0 },	// W - 挖地先生
+	{ "mutnat", 0 },	// W - 外星游龙
+	{ "quizdais", 0 },	// W - 问答大搜查线1 最后倒计时
+	{ "quizdai2", 0 },	// W - 问答大搜查线2 问答迷侦探
+	{ "nitd", 0 },		// W - 午夜噩梦
+	{ "nitdbl", 0 },
+	{ "ganryu", 0 },	// W - 武藏严流记
+	{ "marukodq", 0 },	// X - 小丸子问答
+	{ "matrim", 0 },	// X - 新豪血寺一族 斗婚
+	{ "matrimbl", 1 },
+	{ "zupapa", 0 },	// X - 星星大战
+	{ "joyjoy", 0 },	// Y - 阳光方块
+	{ "galaxyfg", 0 },	// Y - 银河快打 宇宙战士
+	{ "androdun", 0 },	// Y - 勇者之击
+	{ "preisle2", 0 },	// Y - 原始岛2
+	{ "lastblad", 0 },	// Y - 月华剑士1
+	{ "lastbladh", 0 },
+	{ "lastsold", 0 },
+	{ "lastbld2", 0 },	// Y - 月华剑士2
+	{ "nam1975", 0 },	// Y - 越战1975
+	{ "panicbom", 0 },	// Z - 炸弹人方块
+	{ "neobombe", 0 },	// Z - 炸弹人机皇版
+	{ "cyberlip", 0 },	// Z - 战斗边缘
+	{ "sengoku", 0 },	// Z - 战国传承1
+	{ "sengokh", 0 },
+	{ "sengoku2", 0 },	// Z - 战国传承2
+	{ "sengoku3", 0 },	// Z - 战国传承3
+	{ "rbff1", 0 },		// Z - 真饿狼传说1
+	{ "rbff1a", 0 },
+	{ "rbffspec", 0 },	// Z - 真饿狼传说特别版
+	{ "rbff2", 0 },		// Z - 真饿狼传说2 新的来者
+	{ "rbff2h", 0 },
+	{ "rbff2k", 0 },
+	{ "irrmaze", 1 },	// Z - 终极电流急急棒 (有Bug)
+	{ "lresort", 0 },	// Z - 终极手段
+	{ "zedblade", 0 },	// Z - 作战名末日
+	{ "ct2k3sa", 1 },
+	{ "ct2k3sp", 1 },
+	{ "cthd2003", 1 },
 	{ "kf10thep", 1 },
 	{ "kf2k2mp", 1 },
 	{ "kf2k2mp2", 1 },
@@ -329,172 +493,8 @@ constexpr RomListEntry romlist[]
 	{ "kf2k3pl", 1 },
 	{ "kf2k3upl", 1 },
 	{ "kf2k5uni", 1 },
-	{ "kizuna", 0 },
 	{ "kof10th", 1 },
-	{ "kof2000", 0 },
-	{ "kof2000n", 0 },
-	{ "kof2001", 0 },
-	{ "kof2001h", 0 },
-	{ "kof2002", 0 },
-	{ "kof2002b", 1 },
-	{ "kof2003", 0 },
-	{ "kof2003h", 0 },
 	{ "kof2k4se", 1 },
-	{ "kof94", 0 },
-	{ "kof95", 0 },
-	{ "kof95h", 0 },
-	{ "kof96", 0 },
-	{ "kof96h", 0 },
-	{ "kof97", 0 },
-	{ "kof97a", 0 },
-	{ "kof97pls", 0 },
-	{ "kof98", 0 },
-	{ "kof98k", 0 },
-	{ "kof98n", 0 },
-	{ "kof99", 0 },
-	{ "kof99a", 0 },
-	{ "kof99e", 0 },
-	{ "kof99n", 0 },
-	{ "kof99p", 0 },
-	{ "kog", 1 },
-	{ "kotm", 0 },
-	{ "kotm2", 0 },
-	{ "kotmh", 0 },
-	{ "lans2004", 1 },
-	{ "lastblad", 0 },
-	{ "lastbladh", 0 },
-	{ "lastbld2", 0 },
-	{ "lastsold", 0 },
-	{ "lbowling", 0 },
-	{ "legendos", 0 },
-	{ "lresort", 0 },
-	{ "magdrop2", 0 },
-	{ "magdrop3", 0 },
-	{ "maglord", 0 },
-	{ "maglordh", 0 },
-	{ "mahretsu", 0 },
-	{ "marukodq", 0 },
-	{ "matrim", 0 },
-	{ "matrimbl", 1 },
-	{ "miexchng", 0 },
-	{ "minasan", 0 },
-	{ "mosyougi", 0 },
-	{ "ms4plus", 0 },
-	{ "ms5pcb", 1 },
-	{ "ms5plus", 1 },
-	{ "mslug", 0 },
-	{ "mslug2", 0 },
-	{ "mslug3", 0 },
-	{ "mslug3b6", 0 },
-	{ "mslug3h", 0 },
-	{ "mslug4", 0 },
-	{ "mslug5", 0 },
-	{ "mslug5h", 0 },
-	{ "mslugx", 0 },
-	{ "mutnat", 0 },
-	{ "nam1975", 0 },
-	{ "ncombat", 0 },
-	{ "ncombath", 0 },
-	{ "ncommand", 0 },
-	{ "neobombe", 0 },
-	{ "neocup98", 0 },
-	{ "neodrift", 0 },
-	{ "neomrdo", 0 },
-	{ "ninjamas", 0 },
-	{ "nitd", 0 },
-	{ "nitdbl", 0 },
-	{ "overtop", 0 },
-	{ "panicbom", 0 },
-	{ "pbobbl2n", 0 },
-	{ "pbobblen", 0 },
-	{ "pbobblena", 0 },
-	{ "pgoal", 0 },
-	{ "pnyaa", 0 },
-	{ "popbounc", 0 },
-	{ "preisle2", 0 },
-	{ "pspikes2", 0 },
-	{ "pulstar", 0 },
-	{ "puzzldpr", 0 },
-	{ "puzzledp", 0 },
-	{ "quizdai2", 0 },
-	{ "quizdais", 0 },
-	{ "quizkof", 0 },
-	{ "ragnagrd", 0 },
-	{ "rbff1", 0 },
-	{ "rbff1a", 0 },
-	{ "rbff2", 0 },
-	{ "rbff2h", 0 },
-	{ "rbff2k", 0 },
-	{ "rbffspec", 0 },
-	{ "ridhero", 1 },
-	{ "ridheroh", 1 },
-	{ "roboarmy", 0 },
-	{ "rotd", 1 },
-	{ "s1945p", 0 },
-	{ "samsh5sp", 0 },
-	{ "samsh5sph", 0 },
-	{ "samsh5spn", 0 },
-	{ "samsho", 0 },
-	{ "samsho2", 0 },
-	{ "samsho3", 0 },
-	{ "samsho3h", 0 },
-	{ "samsho4", 0 },
-	{ "samsho5", 0 },
-	{ "samsho5b", 1 },
-	{ "samsho5h", 0 },
-	{ "samshoh", 0 },
-	{ "savagere", 0 },
-	{ "sdodgeb", 0 },
-	{ "sengokh", 0 },
-	{ "sengoku", 0 },
-	{ "sengoku2", 0 },
-	{ "sengoku3", 0 },
-	{ "shocktr2", 0 },
-	{ "shocktra", 0 },
-	{ "shocktro", 0 },
-	{ "socbrawl", 0 },
-	{ "socbrawla", 0 },
-	{ "sonicwi2", 0 },
-	{ "sonicwi3", 0 },
-	{ "spinmast", 0 },
-	{ "ssideki", 0 },
-	{ "ssideki2", 0 },
-	{ "ssideki3", 0 },
-	{ "ssideki4", 0 },
-	{ "stakwin", 0 },
-	{ "stakwin2", 0 },
-	{ "strhoop", 0 },
-	{ "superspy", 0 },
-	{ "svc", 0 },
-	{ "svcboot", 1 },
-	{ "svcpcb", 1 },
-	{ "svcpcba", 1 },
-	{ "svcplus", 1 },
-	{ "svcplusa", 1 },
-	{ "svcsplus", 1 },
-	{ "tophuntr", 0 },
-	{ "tophuntra", 0 },
-	{ "tpgolf", 0 },
-	{ "trally", 0 },
-	{ "totcarib", 0 },
-	{ "turfmast", 0 },
-	{ "twinspri", 0 },
-	{ "tws96", 0 },
-	{ "viewpoin", 0	},
-	{ "vliner", 1 },
-	{ "vlinero", 1 },
-	{ "wakuwak7", 0 },
-	{ "wh1", 0 },
-	{ "wh1h", 0 },
-	{ "wh1ha", 0 },
-	{ "wh2", 0 },
-	{ "wh2j", 0 },
-	{ "wh2jh", 0 },
-	{ "whp", 0 },
-	{ "wjammers", 0 },
-	{ "zedblade", 0 },
-	{ "zintrckb", 0 },
-	{ "zupapa", 0 },
 };
 
 static FS::PathString gameFilePath(EmuApp &app, std::string_view name)
